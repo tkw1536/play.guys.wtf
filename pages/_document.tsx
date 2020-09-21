b5bfb5b7073d96a5cd83c3fdc6a306025e0d87da
Tom Wiesing <tkw01536@gmail.com>
2020-09-21 16:52:58 +0200

    Update legal script options

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -12,7 +12,7 @@ export default class MyDocument extends Document {
       <Html lang="en">
         <Head>
         <script async src="https://track.everyone.wtf/tracker.js" data-ackee-server="https://track.everyone.wtf" data-ackee-domain-id="09b365f0-130a-46d2-b7fe-eaf895915c45"></script>
-        <script async src="https://inform.everyone.wtf/legal.min.js?small"></script>
+        <script async src="https://inform.everyone.wtf/legal.min.js?float"></script>
         </Head>
         <body>
           <Main />
