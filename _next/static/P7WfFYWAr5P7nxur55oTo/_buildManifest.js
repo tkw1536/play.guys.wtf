0beac06680f162ec75c84ae804d17c329fc069f3
GitHub Action <tkw1536@users.noreply.github.com>
2023-07-12 19:12:09 +0000

    45f64f1059dad3bab56b1af966e5f29283506db6

---
+++ _next/static/P7WfFYWAr5P7nxur55oTo/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/ae8b236e6dcf5904.css","static/chunks/pages/index-95d0e40d48a0dde3.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
